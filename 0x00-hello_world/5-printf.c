25a5fa0d8f047644e81605828103d656aba8fbfa
mrpiper21 <benbaah104@gmail.com>
2023-03-17 19:19:38 +0000

    my first hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stlib.h>
+#include <string.h>
+/**
+ * main - print code
+ * Return: - returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
