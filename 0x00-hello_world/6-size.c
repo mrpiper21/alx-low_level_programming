49a13d978de3a7c19306050b323dba94f5333f24
mrpiper21 <benbaah104@gmail.com>
2023-03-17 19:46:56 +0000

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - print out function
+ * Return: - returns 0
+ */
+int main(void)
+{
+	printf(sizeof(int));
+	printf(sizeof(char));
+	printf(sizeof(unsigned char));
+	printf(sizeof(short));
+	printf(sizeof(unsigned short));
+	printf(sizeof(unsigned int));
+	printf(sizeof(long));
+	printf(sizeof(unsigned long));
+	return (0);
+}
