b07eed0f6a8cafd36c4f4771ffc1b565484724ca
mrpiper21 <benbaah104@gmail.com>
2023-03-19 02:35:01 +0000

    lower case letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * main - Entry point
- * Description: displays lowercase of alphabets
- * Returns - returns 0
+ * main - Entry point to the code
+ * Discription: 'displays lowercase aphabets'
+ * Returns: - returns 0
  */
 int main(void)
 {
 	int c = 97;
+
 	while (c <= 122)
 	{
 		putchar(c);
