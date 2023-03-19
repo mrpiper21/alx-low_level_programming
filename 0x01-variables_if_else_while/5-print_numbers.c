f4f266e41dc4d42d1d6ee174d041ad1a1f713f29
mrpiper21 <benbaah104@gmail.com>
2023-03-19 04:03:30 +0000

    Base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point to the code
+ *Description: 'displays the numvers of base 10'
+ *Return: returns 0
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b <= 9; b++)
+	{
+		printf("%d", b);
+	}
+	printf("\n");
+	return (0);
+}
