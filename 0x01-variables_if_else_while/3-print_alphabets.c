56fde66b87f9f142e77a1cdc0ed1bfecb9c07140
mrpiper21 <benbaah104@gmail.com>
2023-03-19 03:07:35 +0000

    lower and upercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - Entry to the code
+ *Description: 'displays lower and upercase alphabets'
+ *Return: returns 0
+ */
+int main(void)
+{
+	int c = 97;
+	int b = 65;
+	while (c <= 122)
+	{
+		putchar(c);
+		c++;
+	}
+	while (b <= 90)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
