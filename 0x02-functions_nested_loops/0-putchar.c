3da258f4eac52abaffc9c58b4fa2fd0366c9e42b
mrpiper21 <benbaah104@gmail.com>
2023-03-22 03:55:44 +0000

    ./0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - Entry point; prints _putchar
- * Return: Always 0
+ * main - Entry point
+ * return: Always 0
  */
 int main(void)
 {
-	putchar(95);
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
