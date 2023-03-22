55e5ea7aa816c38362c420a6e014b7ef57c87fa6
mrpiper21 <benbaah104@gmail.com>
2023-03-22 16:51:31 +0000

    void print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print x10 of loercase alphabets
+ */
+void print_alphabet_x10(void)
+{
+	int c = 0;
+	char d;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (d = 97; d <= 122; d++)
+		{
+			_putchar(d);
+		}
+	}_putchar('\n');
+}
