ccf653622d3eaca3b243da1f97124c86c5e68dd8
mrpiper21 <benbaah104@gmail.com>
2023-03-22 19:19:27 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int _print_sign(int n)
+{
+	if (n > 0)
+	{
+	return (1);
+	_putchar('+');
+	}
+	else if (n == 0)
+	{
+	return (0);
+	-putchar('0');
+	}
+	else if (n < 0)
+	{
+	return (-1);
+	_putchar('-');
+	}
+}
