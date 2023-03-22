c3a406d2930ce5256cdc3fd869334a8cb9ec3b98
mrpiper21 <benbaah104@gmail.com>
2023-03-22 01:26:21 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point; prints _putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar(95);
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
