9610db19d3e4569d748ad7feded94019beeb322e
mrpiper21 <125402164+mrpiper21@users.noreply.github.com>
2023-06-20 17:43:28 +0000

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,9 @@
 /**
  * add - Adds two values
  * Int add - initializes the two values
- * @n1 - First num to be computed
+ * @n1: First num to be computed
  * @n2 - Second num to be computed
- * Return: rturns integer value
+ * Return: returns an integer value
  */
 
 int add(int n1, int n2)
