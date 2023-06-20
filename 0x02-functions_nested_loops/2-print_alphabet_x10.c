6ade0449af568c4e21d1b63573573b84190f1f22
mrpiper21 <125402164+mrpiper21@users.noreply.github.com>
2023-06-20 17:31:07 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,8 +7,10 @@ void print_alphabet_x10(void)
 	int c;
 	char d;
 
-	for (c = 0; c <= 9; c++){
-		for (d = 97; d <= 122; d++){
+	for (c = 0; c <= 9; c++)
+	{
+		for (d = 97; d <= 122; d++)
+		{
 			_putchar(d);
 		}
 		_putchar('\n');
