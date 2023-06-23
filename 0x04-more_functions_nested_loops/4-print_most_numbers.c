57ccec98d0b6a6e0acae82830f64de5a7af9175e
piper <benbaah104@gmail.com>
2023-06-23 09:36:36 +0000

    print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,14 @@
 
 void print_most_numbers(void)
 {
-	int c = 48;
-	while (c <= 57)
+	int c;
+	for (c = 48; c <= 57; c++)
 	{
 		if (c == 50 || c == 52)
 		{
 			continue;
 		}
 		_putchar(c);
-		c++;
 	}
 	_putchar('\n');
 }
