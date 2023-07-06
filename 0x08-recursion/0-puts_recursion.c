082cae6be930b55fe70bcb529a43aab9bbed4cdf
piper <benbaah104@gmail.com>
2023-07-06 19:52:47 +0000

    recurssion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,9 +5,9 @@
  */
 
 void _puts_recursion(char *s){
-     if(*s == '\0'){
-	     putchar('\n');
-	     return;
+     if(*s == '\0') { 
+        putchar('\n');
+        return;
      }
      putchar(*s);
      _puts_recursion(s+1);
