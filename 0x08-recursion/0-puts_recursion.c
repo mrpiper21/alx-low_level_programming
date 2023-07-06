ede0f4778eb8b7015957acd3ba92f0515f113bf7
piper <benbaah104@gmail.com>
2023-07-06 19:49:30 +0000

    recurssion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,10 +4,10 @@
  * @s: The string to be printed
  */
 
-void _puts_recursion(char *s) {
-     if(*s == '\0') {
-        putchar('\n');
-        return;
+void _puts_recursion(char *s){
+     if(*s == '\0'){
+	     putchar('\n');
+	     return;
      }
      putchar(*s);
      _puts_recursion(s+1);
